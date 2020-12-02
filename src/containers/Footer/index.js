194d4de76cb2eec09386b0708236659331db0d00
Aleff Rodrigues <aleff2594@gmail.com>
2020-12-02 20:14:24 -0300

    Criando footer

---
+++ src/containers/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Footer from '../../components/Footer';
+
+function FootContainer() {
+  return (
+    <Footer>
+      <Footer.Title>Questions ? Contact us.</Footer.Title>
+      <Footer.Break />
+
+    </Footer>
+  );
+}
+
+export default FootContainer;
