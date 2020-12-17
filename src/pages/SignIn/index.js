d8db6ea5174f8aaab4cdd96bac0f552231e90f1d
Aleff Rodrigues <aleff2594@gmail.com>
2020-12-17 17:01:48 -0300

    Sign In Page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HeaderContainer from '../../containers/Header';
+import FooterContainer from '../../containers/Footer';
+
+import Form from '../../components/Form';
 
 // import { Container } from './styles';
 
 function SignIn() {
-  return <div />;
+  const [emailAddres, setEmailAddress] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState('');
+
+  const isInvalid = password === '' || emailAddres === '';
+  const handleSignin = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignin} method="POST" >
+            <Form.Input
+              placeholder="Email Adress"
+              value={emailAddres}
+              onChange={ ({ target }) => setEmailAddress(target.value)}
+              />
+
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              value={password}
+              autoComplete="off"
+              onChange={ ({ target }) => setPassword(target.value)}
+              />
+
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
 
 export default SignIn;
