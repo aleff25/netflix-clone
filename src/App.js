887e31c66950d557b62f978e2a814819300991de
Aleff Rodrigues <aleff2594@gmail.com>
2020-12-27 12:24:06 -0300

    Criando redirecionamento de rotas

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Browse from './pages/Browse';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
 import * as ROUTES from './constants/routes';
 
 function App() {
+  const { user } = useAuthListener();
+
   return (
     <Router>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <SignIn />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
-        <SignUp />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
+      <Switch>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+          exact
+          >
+          <SignIn />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+          exact
+          >
+          <SignUp />
+        </IsUserRedirect>
+        <ProtectedRoute
+          user={user}
+          path={ROUTES.BROWSE}
+          exact
+          >
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+          exact
+          >
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
